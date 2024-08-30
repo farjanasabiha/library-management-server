02fde7e1d8078b67e6b8977b394b880fb6831ee1
devsabiha <farjanasabiha786@gmail.com>
2024-08-31 01:46:18 +0600

    Add Cookie

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+// Middleware setup
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
+// Cookie options for JWT
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jtwnv2k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,115 +30,161 @@ const client = new MongoClient(uri, {
   },
 });
 
+// // Logger middleware
+// const logger = (req, res, next) => {
+//   console.log("Request Log:", req.method, req.url);
+//   next();
+// };
+
+// Main function to run server and MongoDB connection
 async function run() {
   try {
+    await client.connect();
     const bookCollection = client.db("bookDB").collection("book");
     const borrowedBookCollection = client
       .db("bookDB")
       .collection("borrowedBook");
-
     const bookCategoryCollection = client
       .db("bookDB")
       .collection("book-categories");
 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user, "jwt User");
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365",
+      });
+      console.log( token)
+      res.cookie('token', token, cookieOptions).send({ success: true });
+    });
+
+    // JWT verification middleware
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized Access" });
+        }
+        req.user = decoded;
+      });
+      next();
+    };
+
+    // Routes
     app.get("/bookCategories", async (req, res) => {
-      const cursor = bookCategoryCollection.find();
-      const result = await cursor.toArray();
+      const result = await bookCategoryCollection.find().toArray();
       res.send(result);
     });
-    app.get("/allBooks", async (req, res) => {
-      const cursor = bookCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
+
+    app.get("/allBooks", verifyToken, async (req, res) => {
+      const { email } = req.query;
+      console.log(email, req.user.email);
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+      const books = await borrowedBookCollection
+        .find({ borrowedEmail: email })
+        .toArray();
+      // const books = await bookCollection.find().toArray();
+      res.send(books);
     });
 
     app.get("/getCategoryBooks/:categoryName", async (req, res) => {
       const categoryName = req.params.categoryName;
-      console.log(categoryName);
-      const cursor = bookCollection.find({ category: categoryName });
-      const result = await cursor.toArray();
-      res.send(result);
+      const books = await bookCollection
+        .find({ category: categoryName })
+        .toArray();
+      res.send(books);
     });
 
     app.get("/bookDetails/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await bookCollection.findOne(query);
-      res.send(result);
+      const book = await bookCollection.findOne({ _id: new ObjectId(id) });
+      res.send(book);
+    });
+
+    app.post("/logout", async (req, res) => {
+      console.log(req.body);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
     });
 
     app.post("/books", async (req, res) => {
-      const newList = req.body;
-      const result = await bookCollection.insertOne(newList);
+      const newBook = req.body;
+      const result = await bookCollection.insertOne(newBook);
       res.send(result);
     });
 
     app.put("/updateBooks/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
       const data = req.body;
-      const updateDoc = {
-        $set: {
-          ...data,
-        },
-      };
-      const result = await bookCollection.updateOne(filter, updateDoc);
-      return res.send(result);
+      const result = await bookCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { ...data } }
+      );
+      res.send(result);
     });
 
     app.get("/getBorrowBooks/:email", async (req, res) => {
       const email = req.params.email;
-      const response = await borrowedBookCollection.find({ borrowedEmail: email }).toArray();
-      return res.send(response)
+      const books = await borrowedBookCollection
+        .find({ borrowedEmail: email })
+        .toArray();
+      res.send(books);
     });
 
     app.delete("/returnBooks/:id", async (req, res) => {
       const id = req.params.id;
       const previousBookId = req.query.previousBookId;
-      console.log(id, "Hello", previousBookId);
-      const mainBook = await bookCollection.findOne({_id : new ObjectId(previousBookId)})
-      const updateDoc = {
-        $set: {
-          quantity_of_books: parseInt(mainBook.quantity_of_books) + 1,
-        },
+
+      const mainBook = await bookCollection.findOne({
+        _id: new ObjectId(previousBookId),
+      });
+      if (!mainBook) {
+        return res.status(404).send({ message: "Book not found" });
       }
-      const result = await bookCollection.updateOne(
+
+      await bookCollection.updateOne(
         { _id: new ObjectId(previousBookId) },
-        updateDoc
+        {
+          $set: { quantity_of_books: parseInt(mainBook.quantity_of_books) + 1 },
+        }
       );
 
       const response = await borrowedBookCollection.deleteOne({
-        _id: new ObjectId(id)
+        _id: new ObjectId(id),
       });
-      return res.send(response)
+      res.send(response);
     });
 
     app.put("/bookDetails/:id", async (req, res) => {
       const bookData = req.body;
-      console.log(bookData);
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const findData = await bookCollection.findOne(filter);
-      const updateBookData = {
-        $set: {
-          ...bookData,
-        },
-      };
-      const response = await borrowedBookCollection.insertOne(bookData);
-      const updateDoc = {
-        $set: {
-          quantity_of_books: parseInt(findData.quantity_of_books) - 1,
-        },
-      };
-      const result = await bookCollection.updateOne(filter, updateDoc);
-      res.send(result);
+
+      const book = await bookCollection.findOne(filter);
+      if (!book) {
+        return res.status(404).send({ message: "Book not found" });
+      }
+
+      await borrowedBookCollection.insertOne(bookData);
+      await bookCollection.updateOne(filter, {
+        $set: { quantity_of_books: parseInt(book.quantity_of_books) - 1 },
+      });
+
+      res.send({ message: "Book borrowed successfully" });
     });
 
     console.log("Connected to MongoDB!");
-  } finally {
-    // Do not close the client connection if you're using it persistently
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
